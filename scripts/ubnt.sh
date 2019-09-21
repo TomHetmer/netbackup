fa7f8d5b9813a6c936af90ef3345617180724467
Tomáš Hetmer <TomHetmer@users.noreply.github.com>
2019-09-21 16:19:07 +0200

    Create ubnt.sh

---
+++ scripts/ubnt.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+ARCHIVE='../archive'
+REPO='.'
+
+inotifywait -m $ARCHIVE -e create -e moved_to |
+    while read path action file; do
+        echo "The file '$file' appeared in directory '$path' via '$action'"
+
+        if [[ "$file" =~ ^config\.boot-(.*)\.[0-9]*_[0-9]*$ ]]; then
+            echo "Valid ubnt file, moving it to repo and pushing"
+            mv "$ARCHIVE/$file" "$REPO/${BASH_REMATCH[1]}"
+            git add .
+            git commit -m "Auto-commit by netbackup"
+            git push
+        else
+            echo "Invalid file"
+        fi
+    done
